485e9c93d51fddccb98cc5f5215466441a5f4c12
Zeiyy <81281058+xznsenpai@users.noreply.github.com>
2024-03-04 22:07:15 +0800

    update app.js

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ var api = require('./route/api');
 
 sh.use(logger('dev'));
 sh.get('/', (req, res) => {
-  res.redirect('/playground')
+	res.redirect('/playground')
 })
 sh.use('/', docs);
 sh.use('/api', api);
 
 sh.listen(8080, async () => {
-  console.log({
-    message: 'menyala abangkuh....'
-  })
+	console.log({
+		message: 'menyala abangkuh....'
+	})
 });
